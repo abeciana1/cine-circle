a2d1054f6de16146d0d8ab51274ea9307a932db9
Alex <alex.beciana@gmail.com>
2024-04-20 23:00:39 -0400

    Rendered home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import Home from '@/src/pages/Home'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"  />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
   )
