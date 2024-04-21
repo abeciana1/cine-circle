32481a8da88d17a1e461c986f42ff484fe9b79a0
Alex <alex.beciana@gmail.com>
2024-04-20 21:42:19 -0400

    Added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepBlue: "#2D31FA",
+        crimsonRed: "#DC143C",
+        charcoal: "#36454F",
+        silverSand: "#BFC1C2",
+        amber: "#FFBF00",
+        teal: "#008080",
+        offWhite: "#F8F9FA",
+        indigoDye: "#2B4162",
+        midnightGreen: "#0F4C5C"
+      }
+    },
   },
   plugins: [],
 }
