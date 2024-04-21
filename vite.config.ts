4caf6800cf59f362534e6dc2f974f4258cc72b09
Alex <alex.beciana@gmail.com>
2024-04-21 19:15:51 -0400

    Changed import source to allow test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 import { codecovVitePlugin } from "@codecov/vite-plugin";
@@ -17,4 +17,17 @@ export default defineConfig({
       uploadToken: process.env.CODECOV_TOKEN,
     }),
   ],
+  test: {
+    include: ['**/__test__/**/*.test.{js,jsx,ts,tsx}'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/*.stories.ts',
+      '**/*.config.{js,ts}',
+      '**/.{idea,git,cache,output,temp}/**'
+    ],
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  }
 })
