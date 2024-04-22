95bddecca572180722d167617b4c2545c62b2cf5
Alex <alex.beciana@gmail.com>
2024-04-22 12:17:38 -0400

    Rendered site nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from '@/src/pages/Home'
+import SiteWideNav from '@/src/components/navigation'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <SiteWideNav />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
