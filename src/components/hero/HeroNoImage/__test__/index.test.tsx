a6db068e1b3bc19525b4f483017e5c589d46902b
Alex <alex.beciana@gmail.com>
2024-04-22 10:31:27 -0400

    Fixed failing test with react-router-dom link

--- src/components/hero/HeroNoImage/__test__/index.test.tsx
+++ src/components/hero/HeroNoImage/__test__/index.test.tsx
@@ -1,11 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom';
 import HeroNoImage from '@/src/components/hero/HeroNoImage'
+import { TextTailwindColor } from '@/src/enums'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('HeroNoImage', () => {
     beforeEach(() => {
-        render(<HeroNoImage/>)
+        render(
+            <MemoryRouter>
+                <HeroNoImage
+                    headlineText='Title'
+                    headlineTextColor={TextTailwindColor.Charcoal}
+                    bodyText='body text'
+                    ctaHref='/'
+                    ctaText='Click'
+                    ctaArrow={true}
+                    ctaAllyTitle='Start searching for movies'
+                    ctaRelativePath
+                />
+            </MemoryRouter>
+        )
     })
+
 
     it('should render an H1 heading', () => {
         const heading = screen.getByRole('heading', {
@@ -21,6 +37,6 @@ describe('HeroNoImage', () => {
 
     it('should render a cta button link', () => {
         const ctaButtonLink = screen.getByRole('link')
-        expect(ctaButtonLink).toHaveAttribute('href', '/movies')
+        expect(ctaButtonLink).toHaveAttribute('href', '/')
     })
 })
