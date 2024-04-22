269ce5b111d65844cf87df3177cd1c4af4022181
Alex <alex.beciana@gmail.com>
2024-04-22 10:29:35 -0400

    Created new component props interfaces

--- src/interfaces/components/styled/index.ts
+++ src/interfaces/components/styled/index.ts
@@ -4,3 +4,18 @@ export interface HeadingProps {
     text: string;
     color: TextTailwindColor;
 }
+
+export interface LinkProps {
+    text: string;
+    href: string;
+    relativePath: boolean;
+    title: string;
+}
+
+export interface CTALinkProps extends LinkProps {
+    ctaArrow: boolean;
+}
+
+export interface PageSectionProps {
+    children: React.ReactNode | React.ReactNode[]
+}
