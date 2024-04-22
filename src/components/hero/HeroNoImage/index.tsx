3f8549746b08c7d9eb22b689d5ce22bf75b1a925
Alex <alex.beciana@gmail.com>
2024-04-22 10:31:44 -0400

    Created HeroNoImage component

--- src/components/hero/HeroNoImage/index.tsx
+++ src/components/hero/HeroNoImage/index.tsx
@@ -1,7 +1,38 @@
+import { HeroNoImageProps } from '@/src/interfaces/components/hero'
+import { H1, CTABtnLink } from '@/src/components'
 
-const HeroNoImage = () => {
+const HeroNoImage = ({
+    headlineText,
+    headlineTextColor,
+    bodyText,
+    ctaHref,
+    ctaText,
+    ctaArrow,
+    ctaAllyTitle,
+    ctaRelativePath
+}: HeroNoImageProps) => {
     return(
-        <></>
+        <section className='text-center'>
+            <H1
+                text={headlineText}
+                color={headlineTextColor}
+            />
+            <div
+                data-testid="hero-body"
+                className='py-5 text-xl text-offWhite'
+            >
+                { bodyText }
+            </div>
+            <div className='flex justify-center'>
+                <CTABtnLink
+                    text={ctaText}
+                    href={ctaHref}
+                    ctaArrow={ctaArrow}
+                    title={ctaAllyTitle}
+                    relativePath={ctaRelativePath}
+                />
+            </div>
+        </section>
     )
 }
 
