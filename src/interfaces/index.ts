ef75de18127ecf84b3f3835ed8701a2ccf5ab725
Alex <alex.beciana@gmail.com>
2024-04-22 12:20:14 -0400

    Exported nav component props interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,3 +3,8 @@ export type {
     CTALinkProps,
     PageSectionProps
 } from '@/src/interfaces/components/styled'
+
+export type {
+    NavLinkProps,
+    NavBarProps
+} from '@/src/interfaces/components/navigation'
