c443e87679263fa6401b3d61f26fb0c4382eced8
Alex <alex.beciana@gmail.com>
2024-04-22 10:29:16 -0400

    Removed initial code with PageSection and HeroNoImage components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
-import { H1 } from '@/src/components'
+import {
+    HeroNoImage,
+    PageSection
+} from '@/src/components'
 import { TextTailwindColor } from '@/src/enums'
 
 const Home = () => {
 
     return (
-        <H1 color={TextTailwindColor.CrimsonRed} text='Explore the World of Movies with CineCircle!' />
+        <>
+            <PageSection>
+                <HeroNoImage
+                    headlineText='Explore the World of Movies with CineCircle!'
+                    headlineTextColor={TextTailwindColor.OffWhite}
+                    bodyText="Discover your next favorite movie effortlessly. Search through an extensive database of films, get tailored recommendations, and instantly find out where to watch them online. Dive into a seamless cinematic experience with CineCircle, no sign-up required!"
+                    ctaText="Explore Now"
+                    ctaAllyTitle="link to search for movies"
+                    ctaHref="/movies"
+                    ctaArrow
+                    ctaRelativePath
+                />
+            </PageSection>
+        </>
     )
 }
 
